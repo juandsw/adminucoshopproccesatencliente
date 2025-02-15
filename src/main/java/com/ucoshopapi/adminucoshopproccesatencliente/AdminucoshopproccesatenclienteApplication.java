64cbc82a62788e03224fe7e1974907988b881f3c
JulianDV22 <juliddv1@gmail.com>
2025-02-15 11:31:12 -0500

    Se inicializa el proyecto adminucoshopprocessatencliente

---
+++ src/main/java/com/ucoshopapi/adminucoshopproccesatencliente/AdminucoshopproccesatenclienteApplication.java
@@ -0,0 +1,13 @@
+package com.ucoshopapi.adminucoshopproccesatencliente;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AdminucoshopproccesatenclienteApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AdminucoshopproccesatenclienteApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ucoshopapi/adminucoshopproccesatencliente/AdminucoshopproccesatenclienteApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ucoshopapi.adminucoshopproccesatencliente;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AdminucoshopproccesatenclienteApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
